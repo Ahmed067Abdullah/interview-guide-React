858ad1500981094a0f63cc36a9ef67bb76d543c7
Ahmed067Abdullah <ahmed067abdullah@gmail.com>
2019-11-12 20:40:47 +0500

    check for duplicate names

--- src/containers/Auth/Auth.service.js
+++ src/containers/Auth/Auth.service.js
@@ -2,14 +2,26 @@ import { database, auth } from "firebase";
 import dispatcher from "../../store/dispater";
 import { setUser, clearState } from "./Auth.action";
 
-export const successfullyAuthenticated = (user,history, dispatch) => {
+export const successfullyAuthenticated = (user, history, dispatch) => {
   dispatch(setUser(user));
   localStorage.setItem("interview-guide", JSON.stringify(user));
-  history.replace('/questions');
+  history.replace("/questions");
 };
 
 export const signup = ({ name, email, password }, history) => dispatch =>
-  new Promise((resolve, reject) => {
+  new Promise(async (resolve, reject) => {
+    // check if user with same name already exists
+    const snapshot = await database()
+      .ref("users/")
+      .once("value");
+    const userWithSameName = Object.values(snapshot.val()).find(
+      u => u.name === name
+    );
+    if (userWithSameName) {
+      reject("Name is not available");
+    }
+
+    // created user if name not already taken
     auth()
       .createUserWithEmailAndPassword(email, password)
       .then(res => {
@@ -18,11 +30,15 @@ export const signup = ({ name, email, password }, history) => dispatch =>
           .ref(`users/${uid}`)
           .set({ uid, name, email })
           .then(res => {
-            successfullyAuthenticated({
-              uid,
-              email,
-              name,
-            }, history, dispatch);
+            successfullyAuthenticated(
+              {
+                uid,
+                email,
+                name,
+              },
+              history,
+              dispatch
+            );
           })
           .catch(err => reject("Error occured while storing user info"));
       })
@@ -48,11 +64,15 @@ export const signin = ({ email, password }, history) => dispatch =>
           .once("value")
           .then(res => {
             if (res.val()) {
-              successfullyAuthenticated({
-                uid,
-                email,
-                name: res.val().name,
-              },history, dispatch);
+              successfullyAuthenticated(
+                {
+                  uid,
+                  email,
+                  name: res.val().name,
+                },
+                history,
+                dispatch
+              );
             } else {
               reject("Can't find user details");
             }
@@ -74,7 +94,6 @@ export const signin = ({ email, password }, history) => dispatch =>
   });
 
 export const logout = () => dispatch => {
-  console.log('here')
   localStorage.removeItem("interview-guide");
   dispatcher(dispatch(clearState()));
 };
